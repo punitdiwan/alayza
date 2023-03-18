d37a5cce359c93a63ed345b681d98234309bca18
Ravindra Meena <ravindra@maitretech.com>
2023-03-18 19:06:12 +0530

    done

--- pages/Products/index.js
+++ pages/Products/index.js
@@ -11,18 +11,6 @@ export async function getStaticProps() {
   const data2 = JSON.stringify(data1);
   const data3 = JSON.parse(data2);
 
-  // const data1 = await prisma.product.findMany({
-
-  //     where:{
-  //     category:{
-  //       contains:"powder"
-  //     }
-  // }
-  // })
-  // const data2 = JSON.stringify(data1);
-  // const data3 = JSON.parse(data2);
-  // console.log("revalidating")
-
   return {
     props: {
       data3,
@@ -69,33 +57,6 @@ const Products = ({ data3 }) => {
     <>
       <Layout>
         <section className="products-main">
-          {/* <Carousel
-            className="products-carousel"
-            autoPlay={true}
-            indicators={false}
-          >
-            <Carousel.Item className="carousel_image_new">
-              <img
-                className="d-block w-30 center"
-                src="./images/product-1.jpeg"
-                alt="First slide"
-              />
-            </Carousel.Item>
-            <Carousel.Item className="carousel_image_new">
-              <img
-                className="d-block w-30"
-                src="./images/product-3.jpeg"
-                alt="Second slide"
-              />
-            </Carousel.Item>
-            <Carousel.Item className="carousel_image_new">
-              <img
-                className="d-block w-30"
-                src="./images/product-2.jpeg"
-                alt="Third slide"
-              />
-            </Carousel.Item>
-          </Carousel> */}
           <div className="dropdown">
             <label>Category</label>
             <select
@@ -127,11 +88,6 @@ const Products = ({ data3 }) => {
                           <div className="card_content">
                             <h3>{item.brand}</h3>
                             <h2 className="card_title">{item.name}</h2>
-                            {/* <p className="card_text">
-                              <FaRegStar /> <FaRegStar /> <FaRegStar />
-                              <FaRegStar />
-                              <FaRegStar /> {item.rating} Reviews
-                            </p> */}
                             <h5>Rs. {item.price}</h5>
                           </div>
                         </div>
@@ -150,11 +106,6 @@ const Products = ({ data3 }) => {
                           <div className="card_content">
                             <h3>{item.brand}</h3>
                             <h2 className="card_title">{item.name}</h2>
-                            {/* <p className="card_text">
-                              <FaRegStar /> <FaRegStar /> <FaRegStar />
-                              <FaRegStar />
-                              <FaRegStar /> {item.rating} Reviews
-                            </p> */}
                             <h5>Rs. {item.price}</h5>
                           </div>
                         </div>
@@ -170,48 +121,3 @@ const Products = ({ data3 }) => {
 };
 
 export default Products;
-
-// export const productData = [
-//   {
-//     id: 0,
-//     img: "./images/airpods.jpg",
-//     title: " Airpods Wireless Bluetooth Headphones",
-//     rating: "1",
-//     price: "₹2,495.00",
-//   },
-//   {
-//     id: 1,
-//     img: "./images/phone.jpg",
-//     title: " IPhone 11 Pro 256GB Memory",
-//     rating: "4",
-//     price: "₹1,949.00",
-//   },
-//   {
-//     id: 2,
-//     img: "./images/camera.jpg",
-//     title: " Cannon EOS 80D DSLR Camera",
-//     rating: "6",
-//     price: "₹92,500.00",
-//   },
-//   {
-//     id: 3,
-//     img: "./images/camera.jpg",
-//     title: "Cannon EOS 80D DSLR Camera ",
-//     rating: "2",
-//     price: "₹24,500.00",
-//   },
-//   {
-//     id: 4,
-//     img: "./images/mouse.jpg",
-//     title: "Logitech G-Series Gaming Mouse ",
-//     rating: "4",
-//     price: "₹92,500.00",
-//   },
-//   {
-//     id: 5,
-//     img: "./images/alexa.jpg",
-//     title: " Amazon Echo Dot 3rd Generation",
-//     rating: "5",
-//     price: "₹49,990.00",
-//   },
-// ];
