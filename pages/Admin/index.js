41d063f314d2e03f27b24d543f2093ca45faf4b3
jalam dangi <jdangi@maitretech.com>
2023-04-03 16:38:37 +0530

    update second

--- pages/Admin/index.js
+++ pages/Admin/index.js
@@ -2,6 +2,7 @@ import React from "react";
 // import Header from "../../Components/Header";
 // import Footer from "../../Components/Footer";
 import Products from "../Products";
+import React, { useEffect } from "react";
 // import Layout from "../../Components/Layout";
 
 export async function getStaticProps() {
